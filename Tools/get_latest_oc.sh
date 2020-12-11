925c21ff4918098422ce941475f6ef8bdf3e612a
ray <henrayluo@gmail.com>
2020-12-11 20:08:19 +0800

    Add shell to update oc

---
+++ Tools/get_latest_oc.sh
@@ -0,0 +1,44 @@
+
+rm -rf EFI
+mkdir EFI
+
+sh get_latest_kexts.sh
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+download_oc() {
+    latest_version=$(get_latest_release $1)
+    wget -c "https://github.com/$1/releases/download/$latest_version/$2-$latest_version-RELEASE.zip"
+    unzip $2-$latest_version-RELEASE.zip "X64/EFI/*" -d ./
+    mv X64/EFI ./
+    rm -rf X64
+}
+
+download_oc "acidanthera/OpenCorePkg" OpenCore
+
+# Kexts
+mv Kexts EFI/OC/
+cp -r ../EFI/OC/Kexts/USBMap.kext EFI/OC/Kexts/
+
+# ACPI
+cp -r ../EFI/OC/ACPI EFI/OC/
+
+# Drivers
+cp -r ../EFI/OC/Drivers/HfsPlus.efi EFI/OC/Drivers/HfsPlus.efi
+rm -rf EFI/OC/Drivers/AudioDxe.efi
+rm -rf EFI/OC/Drivers/CrScreenshotDxe.efi
+rm -rf EFI/OC/Drivers/HiiDatabase.efi
+rm -rf EFI/OC/Drivers/NvmExpressDxe.efi
+rm -rf EFI/OC/Drivers/OpenCanopy.efi
+rm -rf EFI/OC/Drivers/OpenUsbKbDxe.efi
+rm -rf EFI/OC/Drivers/Ps2KeyboardDxe.efi
+rm -rf EFI/OC/Drivers/Ps2MouseDxe.efi
+rm -rf EFI/OC/Drivers/UsbMouseDxe.efi
+rm -rf EFI/OC/Drivers/XhciDxe.efi
+
+# rm Resources
+rm -rf EFI/OC/Resources
