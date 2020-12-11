3d3d22414dc68a5ab7c8edaf65bd7445f768e786
ray <henrayluo@gmail.com>
2020-12-11 18:17:32 +0800

    Add shell tool to get latest kexts

---
+++ Tools/get_latest_kexts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Get latest kexts for my hackintosh
+
+# clean up folder
+rm -rf Kexts
+mkdir Kexts
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+download_kext() {
+    latest_version=$(get_latest_release $1)
+    wget -c "https://github.com/$1/releases/download/$latest_version/$2-$latest_version-RELEASE.zip"
+    unzip $2-$latest_version-RELEASE.zip "$2.kext/*" -d Kexts/
+}
+
+download_virtualsmc() {
+    latest_version=$(get_latest_release $1)
+    wget -c "https://github.com/$1/releases/download/$latest_version/$2-$latest_version-RELEASE.zip"
+    unzip $2-$latest_version-RELEASE.zip "Kexts/$3.kext/*" -d ./
+}
+
+download_kext "acidanthera/Lilu" Lilu
+download_kext "acidanthera/AppleALC" AppleALC
+download_kext "acidanthera/WhateverGreen" WhateverGreen
+download_kext "acidanthera/IntelMausi" IntelMausi
+
+download_virtualsmc "acidanthera/VirtualSMC" VirtualSMC VirtualSMC
+download_virtualsmc "acidanthera/VirtualSMC" VirtualSMC SMCProcessor
+download_virtualsmc "acidanthera/VirtualSMC" VirtualSMC SMCSuperIO
